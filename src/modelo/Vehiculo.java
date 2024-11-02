be7d17aa18f70c3985a04bcddb643f2684fe5c33
Luisda <luisda915@gmail.com>
2024-11-02 18:40:35 -0300

    Created metodos abastractos, getters setters y to-string

--- src/modelo/Vehiculo.java
+++ src/modelo/Vehiculo.java
@@ -1,5 +1,39 @@
 package modelo;
 
-public class Vehiculo {
+public abstract class Vehiculo {
+	//atributos
+    private int numeroRuedas;
+    private int cantidadVentanas;
 
+    // Constructor
+    public Vehiculo(int numeroRuedas, int cantidadVentanas) {
+        this.numeroRuedas = numeroRuedas;
+        this.cantidadVentanas = cantidadVentanas;
+    }
+
+    // Métodos abstractos
+    public abstract void encender();
+    public abstract void apagar();
+
+    // Getters y Setters
+    public int getNumeroRuedas() {
+        return numeroRuedas;
+    }
+
+    public void setNumeroRuedas(int numeroRuedas) {
+        this.numeroRuedas = numeroRuedas;
+    }
+
+    public int getCantidadVentanas() {
+        return cantidadVentanas;
+    }
+
+    public void setCantidadVentanas(int cantidadVentanas) {
+        this.cantidadVentanas = cantidadVentanas;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo con " + numeroRuedas + " ruedas y " + cantidadVentanas + " ventanas.";
+    }
 }
