7209dc272e3e74b877e1999d8a7106d1d297ecfd
Luisda <luisda915@gmail.com>
2024-11-02 20:14:44 -0300

    se crean nuevos metodos

--- src/modelo/Bicicleta.java
+++ src/modelo/Bicicleta.java
@@ -5,19 +5,32 @@ import interfaces.interfazVehiculo;
 public class Bicicleta extends Vehiculo implements interfazVehiculo{
 	//un solo atribuo a consideracion. 
     private String tipoBicicleta;
+    private String tipoCombustible; // Electricidad, Bencina, o Fuerza de las piernas
 
     // Constructor
-    public Bicicleta(int numeroRuedas, int cantidadVentanas, String tipoBicicleta) {
-        super(numeroRuedas, cantidadVentanas);
+    public Bicicleta(int numeroRuedas, String tipoBicicleta,String tipoCombustible) {
+        super(numeroRuedas, 0);// se pone 0 porque las bicis no tienen ventanas
         this.tipoBicicleta = tipoBicicleta;
+        this.tipoCombustible = tipoCombustible;
     }
     
 
     // Implementación de los métodos abstractos
     @Override
     public void encender() {
-        System.out.println("Estás listo para pedalear.");
+    	switch (tipoCombustible.toLowerCase()) {
+        case "electricidad":
+            System.out.println("La bicicleta eléctrica está encendida.");
+            break;
+        case "bencina":
+            System.out.println("La bicicleta con motor a bencina está encendida.");
+            break;
+        default:
+            System.out.println("Las bicicletas mecánicas no se encienden, ¡empieza a pedalear!");
+            break;
     }
+}
+    
 
     @Override
     public void apagar() {
@@ -44,5 +57,22 @@ public class Bicicleta extends Vehiculo implements interfazVehiculo{
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+
+	@Override
+	public void cargarBencina() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public String getTipoCombustible() {
+		return tipoCombustible;
+	}
+
+
+	public String setTipoCombustible(String tipoCombustible) {
+	       return "Bicicleta tipo " + tipoBicicleta + " que funciona con " + tipoCombustible + ", " + super.toString();
+	    }
 
 }
