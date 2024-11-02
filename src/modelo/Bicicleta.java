1e76861a9da6b3928a61f1c6688c27698ae05280
Luisda <luisda915@gmail.com>
2024-11-02 19:19:03 -0300

    implements interfaz para quitar error en main

--- src/modelo/Bicicleta.java
+++ src/modelo/Bicicleta.java
@@ -1,5 +1,48 @@
 package modelo;
 
-public class Bicicleta {
+import interfaces.interfazVehiculo;
+
+public class Bicicleta extends Vehiculo implements interfazVehiculo{
+	//un solo atribuo a consideracion. 
+    private String tipoBicicleta;
+
+    // Constructor
+    public Bicicleta(int numeroRuedas, int cantidadVentanas, String tipoBicicleta) {
+        super(numeroRuedas, cantidadVentanas);
+        this.tipoBicicleta = tipoBicicleta;
+    }
+    
+
+    // Implementación de los métodos abstractos
+    @Override
+    public void encender() {
+        System.out.println("Estás listo para pedalear.");
+    }
+
+    @Override
+    public void apagar() {
+        System.out.println("Has terminado de pedalear.");
+    }
+
+    // Getters y Setters
+    public String getTipoBicicleta() {
+        return tipoBicicleta;
+    }
+
+    public void setTipoBicicleta(String tipoBicicleta) {
+        this.tipoBicicleta = tipoBicicleta;
+    }
+
+    @Override
+    public String toString() {
+        return "Bicicleta tipo " + tipoBicicleta + ", " + super.toString();
+    }
+
+
+	@Override
+	public String tipoDeBencina() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
